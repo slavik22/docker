f29e0d8a9c416eb20b82b8a600631f6e81ca1192
Yaroslav Fedyna <fedynaslava@gmail.com>
2024-05-06 18:10:28 +0300

    login component implemented

---
+++ frontend/src/app/login/login.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  loginForm!:FormGroup;
+  constructor(private fb: FormBuilder, private authService: AuthService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ["",Validators.required],
+      password: ["",Validators.required]
+
+    });
+  }
+
+  onSubmit(){
+    if(this.loginForm.valid){
+      this.authService.login(this.loginForm.value).subscribe({
+        next: res => {
+          this.loginForm.reset();
+          this.authService.storeToken(res.token);
+          this.router.navigate(['']);
+        },
+        error: error => {
+          alert(error.message);
+        }
+      })
+    }
+    else{
+      this.loginForm.markAsDirty();
+    }
+  }
+}
